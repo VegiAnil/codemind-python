364c08efc55f0ddd3d1690be185912c8ec9ab33d
VegiAnil <85476203+VegiAnil@users.noreply.github.com>
2022-06-10 08:17:11 +0530

    n=int(input()) s=list(str(n)) for i in range(len(s)):     if s[i]=='6':        s[i]='9'        break st=" " for i in s:     st+=i print(int(st))

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -2,9 +2,9 @@ n=int(input())
 s=list(str(n))
 for i in range(len(s)):
     if s[i]=='6':
-        s[i]='9'
-        break
+       s[i]='9'
+       break
 st=" "
 for i in s:
-   st+=i
-print(int(st))   
+    st+=i
+print(int(st))    
